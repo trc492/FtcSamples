23e2c87297873d402fdcec783c8795925d8541c0
trc492 <trc492@live.com>
2017-10-01 21:22:07 -0700

    Fixed initPeriodic hang. Added getPosition to TrcEnhancedServo class. Added SampleRobot Configuration. Added FtcTeleOpGrabber sample.

---
+++ FtcSampleCode/src/main/java/samples/FtcTeleOpGrabber.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2017 Titan Robotics Club (http://www.titanrobotics.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import ftclib.FtcGamepad;
+import ftclib.FtcOpMode;
+import ftclib.FtcServo;
+import trclib.TrcEnhancedServo;
+
+@TeleOp(name="TeleOp: Dual Servo Grabber", group="3543TeleOpSamples")
+@Disabled
+public class FtcTeleOpGrabber extends FtcOpMode implements FtcGamepad.ButtonHandler
+{
+    private FtcGamepad gamepad;
+    private FtcServo leftServo;
+    private FtcServo rightServo;
+    private TrcEnhancedServo grabber;
+
+    //
+    // Implements FtcOpMode abstract methods.
+    //
+
+    @Override
+    public void initRobot()
+    {
+        hardwareMap.logDevices();
+
+        leftServo = new FtcServo("leftServo");
+        rightServo = new FtcServo("rightServo");
+        grabber = new TrcEnhancedServo("grabber", leftServo, rightServo);
+        leftServo.setInverted(true);
+        rightServo.setInverted(false);
+        //
+        // Initializing gamepads.
+        //
+        gamepad = new FtcGamepad("Gamepad", gamepad1, this);
+        gamepad.setYInverted(true);
+    }   //initRobot
+
+    //
+    // Overrides TrcRobot.RobotMode methods.
+    //
+
+    @Override
+    public void startMode()
+    {
+        //
+        // There is an issue with the gamepad objects that may not be valid
+        // before waitForStart() is called. So we call the setGamepad method
+        // here to update their references in case they have changed.
+        //
+        gamepad.setGamepad(gamepad1);
+    }   //startMode
+
+    @Override
+    public void runPeriodic(double elapsedTime)
+    {
+        grabber.setPosition(gamepad.getRightTrigger());
+    }   //runPeriodic
+
+    //
+    // Implements FtcGamepad.ButtonHandler interface.
+    //
+
+    @Override
+    public void gamepadButtonEvent(FtcGamepad gamepad, int button, boolean pressed)
+    {
+        if (gamepad == this.gamepad)
+        {
+            switch (button)
+            {
+                case FtcGamepad.GAMEPAD_A:
+                    break;
+
+                case FtcGamepad.GAMEPAD_Y:
+                    break;
+
+                case FtcGamepad.GAMEPAD_X:
+                    break;
+
+                case FtcGamepad.GAMEPAD_B:
+                    break;
+
+                case FtcGamepad.GAMEPAD_RBUMPER:
+                    break;
+            }
+        }
+    }   //gamepadButtonEvent
+
+}   //class FtcGrabber
